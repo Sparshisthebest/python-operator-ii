8108c1efb9e70d21da55efd7015ed52c9c434fd3
Sparshisthebest <sparsh.nayak0812@gmail.com>
2025-08-28 11:17:33 +0100

    Add files via upload

---
+++ activity1_5.py
@@ -0,0 +1,15 @@
+#AND-OR
+s=10
+p=12
+a=0
+if s and p and a :
+    print("all the numbers have boolean value as true")
+else :
+    print("at least one number has a false boolean value")
+s=10
+p=-10
+a=0
+if p>0 or a>0:
+    print("either number is greater than 0")
+else:
+    print("no number is greater than 0")
